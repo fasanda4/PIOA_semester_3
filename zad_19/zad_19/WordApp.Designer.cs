8151e153bb831c458943567e60370ef63e4cc157
fasanda4 <fasanda04@gmail.com>
2025-11-17 03:40:41 +0300

    Add files via upload

---
+++ zad_19/zad_19/WordApp.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace zad_19
+{
+    partial class WordApp
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.createATitlePage = new System.Windows.Forms.Button();
+            this.TypeDocument = new System.Windows.Forms.ComboBox();
+            this.workType = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.topicOfWork = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.nameOfTheDiscipline = new System.Windows.Forms.TextBox();
+            this.teacher = new System.Windows.Forms.TextBox();
+            this.createADocument = new System.Windows.Forms.Button();
+            this.CitiesLabel = new System.Windows.Forms.Label();
+            this.CitiesName = new System.Windows.Forms.ComboBox();
+            this.refreshCitiesButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Вид отчетного документа";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(320, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Вид работы";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(320, 261);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(157, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Преподаватель";
+            // 
+            // createATitlePage
+            // 
+            this.createATitlePage.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.createATitlePage.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.createATitlePage.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.createATitlePage.Location = new System.Drawing.Point(199, 446);
+            this.createATitlePage.Name = "createATitlePage";
+            this.createATitlePage.Size = new System.Drawing.Size(172, 102);
+            this.createATitlePage.TabIndex = 15;
+            this.createATitlePage.Text = "Создать титульный лист";
+            this.createATitlePage.UseVisualStyleBackColor = false;
+            this.createATitlePage.Click += new System.EventHandler(this.CreateATitle_Click);
+            // 
+            // TypeDocument
+            // 
+            this.TypeDocument.FormattingEnabled = true;
+            this.TypeDocument.Location = new System.Drawing.Point(31, 107);
+            this.TypeDocument.Name = "TypeDocument";
+            this.TypeDocument.Size = new System.Drawing.Size(202, 33);
+            this.TypeDocument.TabIndex = 16;
+            // 
+            // workType
+            // 
+            this.workType.FormattingEnabled = true;
+            this.workType.Location = new System.Drawing.Point(325, 107);
+            this.workType.Name = "workType";
+            this.workType.Size = new System.Drawing.Size(202, 33);
+            this.workType.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 177);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(149, 25);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Номер работы";
+            // 
+            // number
+            // 
+            this.number.FormattingEnabled = true;
+            this.number.Location = new System.Drawing.Point(31, 205);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(202, 33);
+            this.number.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(320, 177);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(136, 25);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Тема работы";
+            // 
+            // topicOfWork
+            // 
+            this.topicOfWork.Location = new System.Drawing.Point(325, 208);
+            this.topicOfWork.Name = "topicOfWork";
+            this.topicOfWork.Size = new System.Drawing.Size(202, 30);
+            this.topicOfWork.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(26, 261);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(269, 25);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Наименование дисциплины";
+            // 
+            // nameOfTheDiscipline
+            // 
+            this.nameOfTheDiscipline.Location = new System.Drawing.Point(31, 289);
+            this.nameOfTheDiscipline.Name = "nameOfTheDiscipline";
+            this.nameOfTheDiscipline.Size = new System.Drawing.Size(202, 30);
+            this.nameOfTheDiscipline.TabIndex = 23;
+            // 
+            // teacher
+            // 
+            this.teacher.Location = new System.Drawing.Point(325, 289);
+            this.teacher.Name = "teacher";
+            this.teacher.Size = new System.Drawing.Size(202, 30);
+            this.teacher.TabIndex = 24;
+            // 
+            // createADocument
+            // 
+            this.createADocument.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.createADocument.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.createADocument.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.createADocument.Location = new System.Drawing.Point(746, 547);
+            this.createADocument.Name = "createADocument";
+            this.createADocument.Size = new System.Drawing.Size(178, 102);
+            this.createADocument.TabIndex = 26;
+            this.createADocument.Text = "Создать индивидуальный документ";
+            this.createADocument.UseVisualStyleBackColor = false;
+            this.createADocument.Click += new System.EventHandler(this.CreateAIndividualDocument_Click);
+            // 
+            // CitiesLabel
+            // 
+            this.CitiesLabel.AutoSize = true;
+            this.CitiesLabel.Location = new System.Drawing.Point(240, 379);
+            this.CitiesLabel.Name = "CitiesLabel";
+            this.CitiesLabel.Size = new System.Drawing.Size(69, 25);
+            this.CitiesLabel.TabIndex = 28;
+            this.CitiesLabel.Text = "Город";
+            // 
+            // CitiesName
+            // 
+            this.CitiesName.FormattingEnabled = true;
+            this.CitiesName.Location = new System.Drawing.Point(186, 407);
+            this.CitiesName.Name = "CitiesName";
+            this.CitiesName.Size = new System.Drawing.Size(202, 33);
+            this.CitiesName.TabIndex = 29;
+            // 
+            // refreshCitiesButton
+            // 
+            this.refreshCitiesButton.Location = new System.Drawing.Point(395, 407);
+            this.refreshCitiesButton.Name = "refreshCitiesButton";
+            this.refreshCitiesButton.Size = new System.Drawing.Size(132, 33);
+            this.refreshCitiesButton.TabIndex = 30;
+            this.refreshCitiesButton.Text = "Обновить";
+            this.refreshCitiesButton.UseVisualStyleBackColor = true;
+            this.refreshCitiesButton.Click += new System.EventHandler(this.RefreshCities_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::zad_19.Properties.Resources.rrr;
+            this.pictureBox1.Location = new System.Drawing.Point(611, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 529);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // WordApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1099, 699);
+            this.Controls.Add(this.refreshCitiesButton);
+            this.Controls.Add(this.CitiesName);
+            this.Controls.Add(this.CitiesLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.createADocument);
+            this.Controls.Add(this.teacher);
+            this.Controls.Add(this.nameOfTheDiscipline);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.topicOfWork);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.workType);
+            this.Controls.Add(this.TypeDocument);
+            this.Controls.Add(this.createATitlePage);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Name = "WordApp";
+            this.Text = "Задание №19 выполнил: Богомолов В.Н.; Номер варианта: 2; Дата выполнения: 11.11.2" +
+    "025";
+            this.Load += new System.EventHandler(this.WordApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button createATitlePage;
+        private System.Windows.Forms.ComboBox TypeDocument;
+        private System.Windows.Forms.ComboBox workType;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox number;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox topicOfWork;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox nameOfTheDiscipline;
+        private System.Windows.Forms.TextBox teacher;
+        private System.Windows.Forms.Button createADocument;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label CitiesLabel;
+        private System.Windows.Forms.ComboBox CitiesName;
+        private System.Windows.Forms.Button refreshCitiesButton;
+    }
+}
---
+++ zad_19/zad_19/WordApp.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using System.Reflection;
+using Microsoft.Office.Interop.Word;
+
+namespace zad_19
+{
+    public partial class WordApp : Form
+    {
+        private Word.Application wordApp;
+        private Word.Document docTwo;
+
+
+        private readonly string citiesFilePath =
+            System.IO.Path.Combine(System.Windows.Forms.Application.StartupPath, @"..\..\Data\cities.txt");
+
+        object ObjMissing = Missing.Value;
+        private string[] typeDocument = { "Отчёт", "Реферат", "Эссе", "Курсовой проект",
+            "Курсовая работа", "Доклад", "Домашнее задание"};
+        private string[] typeOfWork = { "Лабораторная работа", "Практическая работа", "Индивидуальное задание",
+            "Учебная практика", "Производственная практика", "Преддипломная практика"};
+        private string[] numberOfWork = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+        private string[] Cities = { "Москва", "Санкт-Петербург", "Казань", "Орёл", "Чебоксары", "Тверь", "Старый Оскол", "Нижний Новгород" };
+
+        public WordApp()
+        {
+            InitializeComponent();
+            TypeDocument.Items.AddRange(typeDocument);
+            workType.Items.AddRange(typeOfWork);
+            number.Items.AddRange(numberOfWork);
+            CitiesName.Items.AddRange(Cities);   
+
+
+            LoadCitiesFromFile();
+        }
+
+
+
+        private void LoadCitiesFromFile()
+        {
+            try
+            {
+                if (!File.Exists(citiesFilePath))
+                {
+                    
+                    return;
+                }
+
+                var lines = File.ReadAllLines(citiesFilePath, Encoding.UTF8)
+                                .Select(s => s.Trim())
+                                .Where(s => !string.IsNullOrEmpty(s))
+                                .Distinct()
+                                .ToArray();
+
+                CitiesName.BeginUpdate();
+                CitiesName.Items.Clear();
+                CitiesName.Items.AddRange(lines);
+                CitiesName.EndUpdate();
+
+               
+                if (CitiesName.Items.Count > 0)
+                {
+                    CitiesName.SelectedIndex = 0;
+                }
+                else
+                {
+                    CitiesName.Text = string.Empty;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось загрузить города из файла:\n" + ex.Message,
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void RefreshCities_Click(object sender, EventArgs e)
+        {
+
+            LoadCitiesFromFile();
+        }
+
+        private static void ParagraphText(Paragraph paragraph, string text, int fontSize, string font, string alignment)
+        {
+            paragraph.Range.Text = text;
+            paragraph.Range.Font.Size = fontSize;
+            paragraph.Range.Font.Name = font;
+            switch (alignment)
+            {
+                case "Right":
+                    paragraph.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
+                    break;
+                case "Left":
+                    paragraph.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+                    break;
+                case "Center":
+                    paragraph.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    break;
+                default:
+                    break;
+            }
+            paragraph.Range.InsertParagraphAfter();
+        }
+
+        private void CreateATitle_Click(object sender, EventArgs e)
+        {
+            Word.Document oDoc;
+            Word.Paragraph oPr;
+            Word.Application oWord = new Word.Application();
+            oDoc = oWord.Documents.Add();
+            oPr = oDoc.Paragraphs.Add();
+
+            ParagraphText(oPr, "Министерство транспорта Российской Федерации", 14, "Times new roman", "Center");
+            ParagraphText(oPr, "Федеральное государственное автономное образовательное", 14, "Times new roman", "Center");
+            ParagraphText(oPr, "учреждение высшего образования", 14, "Times new roman", "Center");
+            ParagraphText(oPr, "«Российский университет транспорта»", 14, "Times new roman", "Center");
+            ParagraphText(oPr, "(ФГАОУ ВО РУТ(МИИТ), РУТ (МИИТ)", 14, "Times new roman", "Center");
+
+            oPr.Range.InsertParagraphAfter();
+
+            ParagraphText(oPr, "Институт транспортной техники и систем управления", 14, "Times new roman", "Center");
+
+            oPr.Range.InsertParagraphAfter();
+
+            ParagraphText(oPr, "Кафедра «Управление и защита информации»", 14, "Times new roman", "Center");
+
+            oPr.Range.InsertParagraphAfter();
+            oPr.Range.InsertParagraphAfter();
+            oPr.Range.InsertParagraphAfter();
+            oPr.Range.InsertParagraphAfter();
+            
+
+            string changingText = (TypeDocument.SelectedIndex >= 0 ? TypeDocument.Text : workType.Text) + " №" + number.Text;
+            ParagraphText(oPr, changingText, 28, "Times new roman", "Center");
+
+            oPr.Range.InsertParagraphAfter();
+
+            ParagraphText(oPr, $"по дисциплине: «{nameOfTheDiscipline.Text}»", 14, "Times new roman", "Center");
+
+            oPr.Range.InsertParagraphAfter();
+
+            ParagraphText(oPr, $"на тему: «{topicOfWork.Text}»", 14, "Times new roman", "Center");
+
+            oPr.Range.InsertParagraphAfter();
+            oPr.Range.InsertParagraphAfter();
+
+            ParagraphText(oPr, "Выполнил: ст. гр. ТУУ-211", 14, "Times new roman", "Right");
+            ParagraphText(oPr, "Богомолов В.Н.", 14, "Times new roman", "Right");
+            ParagraphText(oPr, "Вариант №2", 14, "Times new roman", "Right");
+            ParagraphText(oPr, $"Проверил: {teacher.Text}", 14, "Times new roman", "Right");
+
+            oPr.Range.InsertParagraphAfter();
+
+            ParagraphText(oPr, $"{CitiesName.Text} – 2025 г.", 14, "Times new roman", "Center");
+
+            oDoc.SaveAs2(System.Windows.Forms.Application.StartupPath + "\\Титульный лист.docx");
+            oWord.Quit();
+        }
+
+        private void CreateAIndividualDocument_Click(object sender, EventArgs e)
+        {
+            Word.Application oWord2 = new Word.Application();
+            Word.Document oDoc2 = oWord2.Documents.Add();
+            Word.Paragraph oPr2 = oDoc2.Paragraphs.Add();
+
+            ParagraphText(oPr2, "Утверждена", 10, "Times new roman", "Right");
+            ParagraphText(oPr2, "распоряжением Правительства", 10, "Times new roman", "Right");
+            ParagraphText(oPr2, "Российской Федерации", 10, "Times new roman", "Right");
+            ParagraphText(oPr2, "от 26 мая 2005 г. № 667-р", 10, "Times new roman", "Right");
+            ParagraphText(oPr2, "(в ред. от 5 марта 2018 г.) ", 10, "Times new roman", "Right");
+
+            Word.Table table = oDoc2.Tables.Add(oDoc2.Range(113, 113), 4, 5);
+            table.Cell(2, 3).Borders[Word.WdBorderType.wdBorderBottom].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+            table.Cell(3, 3).Borders[Word.WdBorderType.wdBorderBottom].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+            table.Cell(4, 3).Borders[Word.WdBorderType.wdBorderBottom].LineStyle = Word.WdLineStyle.wdLineStyleSingle;
+            table.Cell(1, 1).Merge(table.Cell(1, 5));
+            table.Cell(2, 1).Width = 100;
+            table.Cell(3, 1).Width = 100;
+            table.Cell(4, 1).Width = 100;
+            table.Cell(2, 2).Width = 15;
+            table.Cell(3, 2).Width = 15;
+            table.Cell(4, 2).Width = 15;
+            table.Cell(2, 3).Width = 250;
+            table.Cell(3, 3).Width = 250;
+            table.Cell(4, 3).Width = 250;
+            table.Cell(2, 4).Width = 15;
+            table.Cell(3, 4).Width = 15;
+            table.Cell(4, 4).Width = 15;
+            table.Cell(2, 5).Merge(table.Cell(4, 5));
+            table.Cell(2, 5).Borders.Enable = 1;
+            string[,] data = {
+                {"Анкета", "", "", "",""},
+                {"1. Фамилия", "", "", "", ""},
+                {"Имя", "", "", "", ""},
+                {"Отчество", "", "", "", ""},
+            };
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    try
+                    {
+                        table.Cell(i, j).Range.Text = data[i - 1, j - 1];
+                        table.Cell(i, j).Range.Font.Size = 14;
+                        table.Cell(i, j).Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                    }
+                    catch { }
+                }
+            }
+            table.Cell(2, 5).Range.Text = "Место для фотографии";
+            table.Cell(2, 5).VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+            table.Cell(1, 1).Range.Font.Size = 16;
+            table.Cell(1, 1).Range.Bold = 1;
+
+            Word.Paragraph oPr3 = oDoc2.Paragraphs.Add();
+            oPr3.Range.InsertParagraphAfter();
+            Word.Table newTable = oDoc2.Tables.Add(oPr3.Range, 6, 2);
+            string[,] data2 = {
+                {"2. Если изменяли фамилию, имя или отчество, то укажите их, а также когда, где и по какой причине изменяли", ""},
+                {"3. Число, месяц, год и место рождения (село, деревня, город, район, область, край, республика, страна)", ""},
+                {"4. Гражданство (если изменяли, то укажите, когда и по какой причине, если имеете гражданство другого государства – укажите ", ""},
+                {"5. Образование (когда и какие учебные заведения окончили, номера дипломов) Направление подготовки или специальность по диплому Квалификация по диплому", ""},
+                {"6. Послевузовское профессиональное образование: адьюнктура, докторантура (наименование образовательного или научного учреждения, год окончания)Ученая степень, ученое звание (когда присвоены, номера дипломов, аттестатов)", ""},
+                {"7. Какими иностранными языками и языками народов Российской Федерации владеете и в какой степени (читаете и переводите со словарем, читаете и можете объясняться, владеете свободно)", ""},
+            };
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 1; j <= 2; j++)
+                {
+                    try
+                    {
+                        newTable.Cell(i, j).Range.Text = data2[i - 1, j - 1];
+                        newTable.Cell(i, j).Range.Font.Size = 12;
+                        newTable.Cell(i, j).Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
+                    }
+                    catch { }
+                }
+            }
+            newTable.Borders.Enable = 1;
+            newTable.Borders[Word.WdBorderType.wdBorderLeft].LineStyle = Word.WdLineStyle.wdLineStyleNone;
+            newTable.Borders[Word.WdBorderType.wdBorderRight].LineStyle = Word.WdLineStyle.wdLineStyleNone;
+            oDoc2.SaveAs2(System.Windows.Forms.Application.StartupPath + "\\Индивидуальный документ.docx");
+            oWord2.Quit();
+        }
+
+        private void WordApp_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
